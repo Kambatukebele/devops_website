b13bcd04d6af35c40d93047c11ab0caf4e399f94
Kambatukebele <kambacharles11@gmail.com>
2025-02-27 22:11:36 +0100

    added tailwindcss v3.. and google font['kanit', 'Space grotesk']

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        Kanit: ["Kanit", "sans-serif"],
+        SpaceGrotesk: ["Space Grotesk", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};

28d70df6e941765d1baecdee10b17665e06d0c77
Kambatukebele <kambacharles11@gmail.com>
2025-03-05 09:18:45 +0100

    created about folder and component

---
+++ src/components/about/About.jsx
@@ -0,0 +1,23 @@
+const About = () => {
+  return (
+    <section className="w-full h-fit py-6 lg:py-12">
+      <div className="container px-4 mx-auto">
+        <div>
+          <h2 className="text-xl leading-loose font-Kanit font-light sm:text-3xl sm:leading-normal lg:leading-[50px]">
+            <span className="px-2 bg-black text-white">We are qubitz</span>, a
+            passionate team of DevOps experts with years of industry experience.
+            Our mission is to{" "}
+            <span className="px-2 bg-black text-white">help</span> businesses
+            streamline their development processes, modernize their
+            infrastructure, and achieve sustainable{" "}
+            <span className="px-2 bg-black text-white">growth</span>. We partner
+            with you to bring clarity, efficiency, and innovation to your IT
+            operations.
+          </h2>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;

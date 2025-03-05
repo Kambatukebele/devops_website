3ad6a0ae0fa7ae5033de3193dd5f7857e58c1da9
Kambatukebele <kambacharles11@gmail.com>
2025-03-05 10:51:57 +0100

    updated Services.jsx by adding more padding and changing the bacground color. ServiceCard updated was to add more services in the array services

--- src/components/Services/ServiceCard.jsx
+++ src/components/Services/ServiceCard.jsx
@@ -1,16 +1,19 @@
 const ServiceCard = ({ id, icon, servicesName, serviceDetails }) => {
   return (
-    <div className="flex flex-col justify-center items-start gap-4 border-r border-r-gray-200">
+    <div className="flex border px-8 py-4 rounded-md shadow flex-col justify-start items-start gap-4 border-r border-r-gray-200">
       <div>
         <img src={icon} alt={servicesName} />
       </div>
-      <h3 className="font-beatrice text-2xl font-semibold text-gray-900">
-        {servicesName}
-      </h3>
+      <h3 className="text-2xl font-semibold text-blue-900">{servicesName}</h3>
       <ul className="flex flex-col gap-2 justify-center font-light items-start">
-        {serviceDetails.map((details) => {
+        {serviceDetails.map((details, index) => {
           return (
-            <li className="font-Kanit text-base list-disc ml-5">{details}</li>
+            <li
+              key={index}
+              className="text-base text-gray-800 font-normal list-disc list-inside"
+            >
+              {details}
+            </li>
           );
         })}
       </ul>
--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -8,58 +8,100 @@ const services = [
   {
     id: 1,
     icon: iconServiceOne,
-    servicesName: "Datacenter Infrastructure Field Support",
+    servicesName: "DevOps automation",
     serviceDetails: [
-      "Servers",
-      "Firewalls",
-      "Routers",
-      "Switches",
-      "Rack and Cabling",
+      "Infrastructure as Code",
+      "CI/CD",
+      "Serverless adoption",
+      "Improved collaboration",
+      "Containerization & Orchestration",
+      "Reduced operational tasks",
     ],
   },
   {
     id: 2,
     icon: iconServiceTwo,
-    servicesName: "DevOps Integration",
-    serviceDetails: ["CI/CD", "Kubernety", "Docker", "Linux", "Git"],
+    servicesName: "Audit and consulting",
+    serviceDetails: [
+      "Aws consulting",
+      "Azure consulting",
+      "Cloud cost audi",
+      "DevOps team mentoring",
+      "Infrastructure modernization",
+      "Test automation review",
+    ],
   },
   {
     id: 3,
     icon: iconServiceThree,
-    servicesName: "Custom development",
+    servicesName: "Cloud migration",
     serviceDetails: [
-      "Front-end",
-      "Back-end",
-      "Web App",
-      "Mobile App",
-      "Reactjs, nextjs",
+      "Legacy application migration",
+      "Risk mitigation",
+      "Ongoing support",
+      "Cloud-to-cloud migration",
+      "Security improvements",
+      "Multi-cloud expertise",
     ],
   },
   {
     id: 4,
     icon: iconServiceFour,
-    servicesName: "UI Design",
+    servicesName: "Logs management, monitoring, and alerting",
     serviceDetails: [
-      "User interface",
-      "User experience",
-      "Design System",
-      "wireframe",
-      "prototype",
+      "Logs collection, parsing, and transformation",
+      "Real-time alerting",
+      "Log & metrics visualization",
+      "24/7 system health monitoring",
+    ],
+  },
+  {
+    id: 5,
+    icon: iconServiceFour,
+    servicesName: "Deployment of large-scale K8s clusters",
+    serviceDetails: [
+      "Faster development with standard Terraform project structure",
+      "Minimized architecture-related security risks",
+      "Unlimited auto-scaling",
+      "Easy container orchestration",
+      "Savings with K8s & Spot Instances",
+    ],
+  },
+  {
+    id: 6,
+    icon: iconServiceFour,
+    servicesName: "Migration to GitLab for easy compliance",
+    serviceDetails: [
+      "Easy on-premise GitLab deployment",
+      "Integration with self-hosted runners to save costs",
+      "Simplified compliance and audit trails",
+      "Lower business risks",
+    ],
+  },
+  {
+    id: 7,
+    icon: iconServiceFour,
+    servicesName: "Cloud cost optimization",
+    serviceDetails: [
+      "Cloud infrastructure audit",
+      "Architecture optimization",
+      "Resource rightsizing",
+      "Cloud storage optimization ",
     ],
   },
 ];
 const Services = () => {
   return (
-    <section className="w-full h-fit">
-      <div className="container px-4 mx-auto py-6 lg:py-12">
-        <div className="flex flex-col justify-center items-start gap-4 py-6 lg:flex-row mb-6 lg:mb-12">
+    <section className="w-full h-fit bg-white py-8 lg:py-16">
+      <div className="container px-4 mx-auto flex flex-col justify-start items-start gap-8 lg:gap-32 lg:items-center">
+        <div className="flex flex-col justify-center items-start gap-4 w-full lg:flex-row">
           <div className="lg:w-1/2">
-            <h2 className="font-beatrice text-4xl font-semibold text-gray-900 md:text-5xl lg:text-6xl">
-              Our exclusive services
+            <h2 className="text-4xl font-bold text-blue-900 md:text-5xl lg:text-6xl">
+              Our exclusive <br /> services
             </h2>
           </div>
           <div className="lg:w-1/2">
-            <p className="font-Kanit text-base font-normal text-gray-700 lg:w-[450px] lg:ml-auto">
+            <p className="text-base font-normal text-gray-800 lg:w-[450px] lg:ml-auto">
               Whether it's Crafting a visually stunning brand identity,
               designing immersive digital experiences, or developing strategic
               marketing campaigns

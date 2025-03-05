d90bef5fba0a6b39f820b785d801c7c3de00ee68
Kambatukebele <kambacharles11@gmail.com>
2025-03-05 16:16:21 +0100

    updated the padding and the bg color

--- src/components/text-image/TextImage.jsx
+++ src/components/text-image/TextImage.jsx
@@ -1,50 +1,32 @@
 import compare from "../../assets/images/compare.webp";
 // the reverseRow is just to reverse the image and image position
-const TextImage = ({ reverseRow }) => {
-  console.log(reverseRow);
-
+const TextImage = () => {
   return (
-    <section className="w-full h-full py-6 lg:py-12">
-      <div
-        className={`container mx-auto px-4 flex ${
-          reverseRow
-            ? "flex-col-reverse lg:flex-row-reverse"
-            : "flex-col lg:flex-row"
-        }  justify-center items-start gap-9`}
-      >
-        <div className="w-full flex flex-col justify-center items-start gap-4 lg:w-1/2">
-          <h2 className="text-3xl font-semibold font-beatrice text-gray-900 md:text-4xl lg:text-5xl">
-            Creating one of the fastest way to grow your business successfully
-          </h2>
-          <p className="text-base font-light font-Kanit text-gray-700 md:text-lg">
-            We believe that the human dimensions essential to start any
-            successful project and that this is where splendid emotional
-            relationships between the company and people are born
-          </p>
-          <ul className="flex flex-col justify-center items-start list-decimal list-inside gap-2">
-            <li className="text-gray-900 font-beatrice font-semibold">
-              Visual project management
-            </li>
-            <li className="text-gray-900 font-beatrice font-semibold">
-              Team collaboration
-            </li>
-            <li className="text-gray-900 font-beatrice font-semibold">
-              Connection to company
-            </li>
-            <li className="text-gray-900 font-beatrice font-semibold">
-              Connection to company
-            </li>
-            <li className="text-gray-900 font-beatrice font-semibold">
-              Connection to company
-            </li>
-          </ul>
-        </div>
-        <div className="w-full h-full lg:w-1/2">
-          <img
-            className="w-full h-full object-cover object-center"
-            src={compare}
-            alt="image office"
-          />
+    <section className="w-full h-full bg-gray-50 py-8 lg:py-16">
+      <div className="container px-4 mx-auto flex flex-col justify-start items-start lg:items-center">
+        <div className="flex flex-col justify-start items-start gap-6 lg:flex-row xl:items-center">
+          <div className="w-full flex flex-col justify-center items-start gap-4 lg:w-1/2">
+            <h2 className="text-3xl font-semibold text-blue-900 md:text-4xl lg:text-5xl">
+              DevSecOps
+            </h2>
+            <p className="text-base font-normal text-gray-800 md:text-lg">
+              We integrate security directly into the DevOps pipeline, making it
+              a core part of development instead of an afterthought. DevSecOps
+              leverages automation to drive agility, security, and speed,
+              ensuring continuous compliance and reducing vulnerabilities
+              throughout the software development lifecycle.
+            </p>
+            <a className="bg-blue-900 text-white py-4 px-8 rounded-md text-base shadow-2xl cursor-pointer">
+              Contact us
+            </a>
+          </div>
+          <div className="w-full h-full rounded-md lg:w-1/2">
+            <img
+              className="w-full h-full object-cover object-center rounded-md"
+              src={compare}
+              alt="image office"
+            />
+          </div>
         </div>
       </div>
     </section>

8ccd94856332f450824c33a05842867910a31d11
Kambatukebele <kambacharles11@gmail.com>
2025-03-05 10:09:06 +0100

    updated about.jsx but adding padding py-8 on small screen and py-24 on large screen, and changed the color background to white and blue-800 on the highlighted element

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,19 +1,19 @@
 const About = () => {
   return (
-    <section className="w-full h-fit py-6 lg:py-12">
+    <section className="w-full bg-white h-fit py-8 lg:py-16">
       <div className="container px-4 mx-auto">
         <div>
-          <h2 className="text-xl leading-loose font-Kanit font-light sm:text-3xl sm:leading-normal lg:leading-[50px]">
-            <span className="px-2 bg-black text-white">We are qubitz</span>, a
-            passionate team of DevOps experts with years of industry experience.
-            Our mission is to{" "}
-            <span className="px-2 bg-black text-white">help</span> businesses
+          <h3 className="text-xl text-gray-900 leading-loose font-light sm:text-3xl sm:leading-normal lg:leading-[50px]">
+            <span className="px-2 bg-blue-900 text-white">We are qubitz</span>,
+            a passionate team of DevOps experts with years of industry
+            experience. Our mission is to{" "}
+            <span className="px-2 bg-blue-900 text-white">help</span> businesses
             streamline their development processes, modernize their
             infrastructure, and achieve sustainable{" "}
-            <span className="px-2 bg-black text-white">growth</span>. We partner
-            with you to bring clarity, efficiency, and innovation to your IT
-            operations.
-          </h2>
+            <span className="px-2 bg-blue-900 text-white">growth</span>. We
+            partner with you to bring clarity, efficiency, and innovation to
+            your IT operations.
+          </h3>
         </div>
       </div>
     </section>

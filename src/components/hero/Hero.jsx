1f5c9d1d327f5f05abc52a854a83095a4d7d1b9a
Kambatukebele <kambacharles11@gmail.com>
2025-04-20 11:15:18 +0200

    updated the hero by adding custom blue color and the size of button and description

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,30 +1,29 @@
-import arrow from "../../assets/icons/arrow.svg";
 
 const Hero = () => {
   return (
     <section className="w-full h-fit py-12 lg:py-24">
       <div className="container  mx-auto px-4  lg:flex lg:justify-between lg:items-start lg:gap-5">
-        <div className="w-full flex flex-col gap-4 justify-center items-center lg:gap-6">
+        <div className="w-full flex flex-col gap-4 justify-center items-center">
           <div className="w-full">
-            <h1 className="text-4xl text-left text-blue-950 font-bold leading-10 sm:text-center md:text-5xl lg:text-center lg:w-full lg:text-6xl lg:leading-tight">
+            <h1 className="text-4xl text-left text-gray_900 font-bold leading-10 sm:text-center md:text-5xl lg:text-center lg:w-full lg:text-6xl lg:leading-tight">
               DevOps Consulting Services
             </h1>
           </div>
           <div className="sm:w-[550px] sm:text-center mx-auto md:w-[650px] lg:w-[800px]">
-            <p className="text-base font-normal text-gray-800 sm:text-center md:text-xl lg:text-2xl">
+            <p className="text-base font-normal text-gray_900 sm:text-center md:text-xl">
               Solve any non-trivial challenge with DevOps consulting services
               that treat your delivery pipeline like a product.
             </p>
           </div>
           <div className="w-full items-center justify-center flex flex-col gap-2 sm:flex-row">
             <a
-              className="block bg-blue-900 w-full rounded-md shadow-2xl text-white px-6 py-3 text-base normal-case cursor-pointer lg:text-lg sm:w-60 text-center hover:bg-blue-950 hover:text-white transition ease-out duration-300"
+              className="block bg-blue_600 w-full text-white px-6 py-3 text-base normal-case cursor-pointer font-medium sm:w-48 text-center hover:bg-blue_700 hover:text-white transition ease-out duration-300 rounded-md"
               href="#"
             >
               Free consultation
             </a>
             <a
-              className="block bg-transparent border border-blue-950 w-full rounded-md  text-blue-950 px-6 py-3 text-base normal-case cursor-pointer lg:text-lg sm:w-60 text-center hover:bg-blue-950 hover:text-white transition ease-out duration-300"
+              className="block bg-blue_200 w-full text-blue_600 px-6 py-3 text-base normal-case cursor-pointer font-medium sm:w-48 text-center hover:bg-blue-100 transition ease-out duration-300 rounded-md"
               href="#"
             >
               Contact us

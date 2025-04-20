72474f18d74ebb1fd11d916d3a1f230c207afa49
Kambatukebele <kambacharles11@gmail.com>
2025-04-20 13:01:45 +0200

    upted the text by changing the color blue to blue_600 and removed font-medium css

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -3,16 +3,16 @@ const About = () => {
     <section id="about" className="w-full bg-white h-fit py-12 lg:py-24">
       <div className="container px-4 mx-auto">
         <div>
-          <h3 className="text-xl text-gray-800 leading-loose font-normal sm:text-3xl sm:leading-normal lg:leading-[50px]">
+          <h3 className="text-xl text-gray_900 leading-loose font-normal sm:text-3xl sm:leading-normal lg:leading-[50px]">
             Since 2018,
-            <span className="px-2 font-semibold">qubitz</span>, our DevOps
+            <span className="px-2">qubitz</span>, our DevOps
             consulting firm has collaborated with enterprises and scale-ups,
             providing expertise across
-            <span className="px-2 bg-blue-900 text-white">170+ projects</span>.
+            <span className="px-2 bg-blue_600 text-white">170+ projects</span>.
             Our engineers excel in
-            <span className="px-2 font-semibold">serverless architectures</span>
-            ,<span className="px-2 font-semibold">Kubernetes</span>, and
-            <span className="px-2 font-semibold">on-premise deployments</span>.
+            <span className="px-2">serverless architectures</span>
+            ,<span className="px-2">Kubernetes</span>, and
+            <span className="px-2">on-premise deployments</span>.
           </h3>
         </div>
       </div>
